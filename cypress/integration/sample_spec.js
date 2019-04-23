793f7759e1bc7bf5a477e8751ebc2b4bca7212b6
Gagandeep Singh <gagan.ddun@gmail.com>
2019-04-23 21:16:38 +1000

    added new tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,17 @@
 describe("Testing the home page for automationpractice", () => {
     it("Visits the automationpractice", () => {
-        cy.visit("http://automationpractice.com/index.php");
-    })
+        cy.visit("http://automationpractice.com/index.php")
+    });
+
+    it("Find the content '0123-456-789'", () => {
+        cy.contains('0123-456-789')
+    });
 });
+
+describe("Test the Sign in / Sign up page", () => {
+    it('Should click on sign in link and open the new page', () => {
+        it("Open the sign in page", () => {
+            cy.contains('Sign in').click()
+        })
+    })
+})
