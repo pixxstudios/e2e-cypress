d5245419a7bf987385f181cce83402a22471d27d
Gagandeep Singh <gagan.ddun@gmail.com>
2019-04-24 14:40:22 +1000

    added tests for search functionality

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -6,6 +6,23 @@ describe("Testing the home page for automationpractice", () => {
     it("Find the content '0123-456-789'", () => {
         cy.contains('0123-456-789')
     });
+
+    it("Find the content 'Call us now'", () => {
+        cy.contains('Call us now')
+    });
+});
+
+describe("Test the search functionality", () => {
+    it('Should perform search operation', () => {
+        cy.get('input.search_query')
+        .type('clothes')
+
+        cy.get('button.button-search')
+        .click();
+
+        cy.url()
+        .should('include', 'earch_query=clothes')
+    })
 });
 
 describe("Test the Sign in / Sign up page", () => {
