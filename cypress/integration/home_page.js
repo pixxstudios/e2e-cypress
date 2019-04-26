89e70846d1b2599fcb4b673134d0932b847889af
Gagandeep Singh <gagan.ddun@gmail.com>
2019-04-26 19:35:19 +1000

    added tests for view more and quantity

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -29,10 +29,57 @@ describe("Test the search functionality", () => {
 });
 
 describe("Buy item", () => {
-    it("Should add item to cart after search", () => {
-    cy.contains('Add to cart')
-    .click()
+    it("View more details of the item", () => {
+        cy.get('ul.product_list > li')
+        .click()
+        .contains('More')
+        .click()
     })
+
+    it('Check for product details and price', () => {
+        cy.contains('Faded Short Sleeve T-shirts')
+        cy.contains('Model')
+        cy.contains('demo_1')
+        cy.contains('Condition')
+        cy.contains('New')
+        cy.contains("Faded short sleeve t-shirt with high neckline. Soft and stretchy material for a comfortable fit. Accessorize with a straw hat and you're ready for summer!")
+        cy.contains('$16.51')
+    })
+
+    it('Try changing the quantity', () => {
+        cy.contains('Quantity');
+
+        cy.get('input#quantity_wanted').as('qty');
+        cy.get('@qty').should('have.value', '1');
+
+        cy.get('a.button-plus').click();
+        cy.get('@qty').should('have.value', '2');
+
+        cy.get('a.button-minus').click();
+        cy.get('@qty').should('have.value', '1');
+    })
+
+    it("Search for social links", () => {
+        cy.contains('Tweet')
+        cy.contains('Share')
+        cy.contains('Google+')
+        cy.contains('Pinterest')
+    })
+
+    it("Search email and print links", () => {
+        cy.contains('Send to a friend')
+        cy.contains('Print')
+    })
+
+    it("Should add item to cart after search", () => {
+        cy.contains('Add to cart')
+        .click()
+    })
+
+    /* it("open cart", () => {
+        cy.get('div.shopping_cart > a')
+        .click()
+    }) */
 });
 
 /* describe("Test the Sign in / Sign up page", () => {
