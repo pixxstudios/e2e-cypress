f8d659a5d2dbb2ac095a72281266249b658d5bd0
Gagandeep Singh <gagan.ddun@gmail.com>
2019-04-27 23:00:36 +1000

    few tests added for home page

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -3,6 +3,11 @@ describe("Testing the home page for automationpractice", () => {
         cy.visit("http://automationpractice.com/index.php")
     });
 
+    it('check for image in the header', () => {
+        cy.get('div.banner img')
+        .should('have.attr', 'src' ,'http://automationpractice.com/modules/blockbanner/img/sale70.png');
+    });
+
     it("Check the contact us", () => {
         cy.get('span.shop-phone').as('shopPhone');
 
@@ -19,12 +24,34 @@ describe("Testing the home page for automationpractice", () => {
         cy.get('div#contact-link > a')
         .contains('Contact us')
         .should('have.attr', 'href', 'http://automationpractice.com/index.php?controller=contact')
-        .and('have.attr' ,'title', 'Contact Us')
+        .and('have.attr' ,'title', 'Contact Us');
     });
 
     it("Find the content 'Sign in'", () => {
-        cy.contains('Sign in');
+        cy.get('div.header_user_info > a')
+        .contains('Sign in')
+        .should('have.attr', 'href', 'http://automationpractice.com/index.php?controller=my-account')
+        .and('have.attr' ,'title', 'Log in to your customer account');
     });
+
+    it('homepage slider should have 5 images', () => {
+        cy.get('div#homepage-slider ul > li')
+        .should('have.length', '5');
+    })
+
+    it('homepage slider description', () => {
+        cy.get('div#homepage-slider ul > li').eq(0).as('listItem');
+        
+        cy.get('@listItem').within(() => {
+            cy.get('a')
+            .should('have.attr', 'href', 'http://www.prestashop.com/?utm_source=v16_homeslider')
+            
+            cy.contains('div.homeslider-description h2', 'EXCEPTEUR');
+
+            cy.contains('div.homeslider-description p:first',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique in tortor et dignissim. Quisque non tempor leo. Maecenas egestas sem elit');
+        })
+    })
 
     it("Test the logo", () => {
         cy.get('div#header_logo > a').as('logo');
@@ -32,5 +59,5 @@ describe("Testing the home page for automationpractice", () => {
         cy.get('@logo')
         .should('have.attr', 'href', 'http://automationpractice.com/')
         .and('have.attr', 'title', 'My Store')
-    })
+    });
 });
